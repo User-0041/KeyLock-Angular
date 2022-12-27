f49a90b212d04168046855e8861cff7319fdc013
naztarhex <qhmed0826@gmail.com>
2022-12-27 21:19:06 +0100

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
+
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'landing',
+    pathMatch:'full'
+  },
+  {
+    path:'landing',
+    loadChildren:()=>import('./landing/landing-routing.module').then(m=>m.LandingRoutingModule)
+  },
+  {
+    path:'admin',
+    loadChildren:()=>import('./admin/admin.module').then(m=>m.AdminModule),
+    canActivate:[AuthGuard],
+    data:{roles:["admin","user"]}
+  },
+  {
+    path:'user',
+    loadChildren:()=>import('./user/user.module').then(m=>m.UserModule),
+    canActivate:[AuthGuard],
+    data:{roles:["user"]}
+  }
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
